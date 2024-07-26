dee5b653cb8db02840eb746fa7a3e79d70b287d0
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-26 22:06:39 +0300

    feat(movie_bloc): create movie bloc

---
+++ lib/business_logic/blocs/movie_bloc/movie_bloc.dart
@@ -0,0 +1,35 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:movie_app/data/models/movie.dart';
+import 'package:movie_app/data/models/movie_details.dart';
+import 'package:movie_app/data/repositories/movie_repository.dart';
+
+part 'movie_event.dart';
+part 'movie_state.dart';
+
+class MovieBloc extends Bloc<MovieEvent, MovieState> {
+  final MovieRepository movieRepository;
+
+  MovieBloc(this.movieRepository) : super(MovieInitial()) {
+    on<FetchMoviesEvent>((event, emit) async {
+      emit(MovieLoading());
+      try {
+        final movies = await movieRepository.fetchTrendingMovies(event.page);
+        emit(MovieLoaded(movies: movies));
+      } catch (e) {
+        emit(MovieError(message: e.toString()));
+      }
+    });
+
+    on<FetchMovieDetailsEvent>((event, emit) async {
+      emit(MovieDetailsLoading());
+      try {
+        final movieDetials = await movieRepository.fetchMovieDetails(event.id);
+        emit(MovieDetailsLoaded(movieDetails: movieDetials));
+      } catch (e) {
+        emit(MovieError(message: e.toString()));
+      }
+    });
+  }
+}
