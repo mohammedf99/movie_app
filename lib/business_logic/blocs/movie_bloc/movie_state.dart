04a2a2af4e5d9a0d28194bc350c28bf5b6a6a9a4
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-26 22:05:06 +0300

    feat(movie_state): create different movie states

---
+++ lib/business_logic/blocs/movie_bloc/movie_state.dart
@@ -0,0 +1,42 @@
+part of 'movie_bloc.dart';
+
+@immutable
+abstract class MovieState extends Equatable {
+  const MovieState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class MovieInitial extends MovieState {}
+final class MovieLoading extends MovieState {}
+
+class MovieLoaded extends MovieState {
+  final List<Movie> movies;
+
+  const MovieLoaded({required this.movies});
+
+  @override
+  List<Object> get props => [movies];
+}
+
+class MovieDetailsLoading extends MovieState {}
+
+class MovieDetailsLoaded extends MovieState {
+  final MovieDetails movieDetails;
+
+  const MovieDetailsLoaded({required this.movieDetails});
+
+  @override
+  List<Object> get props => [movieDetails];
+}
+
+class MovieError extends MovieState {
+  final String message;
+
+  const MovieError({required this.message});
+
+  @override
+  List<Object> get props => [message];
+}
+
