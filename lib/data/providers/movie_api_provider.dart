f9a8f997c0492bf2fcf4549a647e85bfe1a390ba
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-26 22:09:00 +0300

    feat(movieApiProvider): create methods to fetch movies and their detials from tmdb

---
+++ lib/data/providers/movie_api_provider.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movie_app/data/models/movie.dart';
+import 'package:http/http.dart' as http;
+import 'package:movie_app/data/models/movie_details.dart';
+
+class MovieApiProvider {
+  Future<List<Movie>> fetchTrendingMovies(int page) async {
+    final response = await http.get(Uri.parse(
+        "https://api.themoviedb.org/3/discover/movie?api_key=${dotenv.env["APIKEY"]}&sort_by=popularity.desc&page=$page"));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<Movie> movies = List<Movie>.from(
+        data['results'].map(
+          (movie) => Movie.fromJson(movie),
+        ),
+      );
+      return movies;
+    } else {
+      throw Exception("Failed to load movies");
+    }
+  }
+
+  Future<MovieDetails> fetchMovieDetails(int id) async {
+    await dotenv.load(fileName: ".env");
+    final response = await http.get(
+      Uri.parse(
+          "https://api.themoviedb.org/3/movie/$id?api_key=${dotenv.env["APIKEY"]}"),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return MovieDetails.fromJson(data);
+    } else {
+      throw Exception("Failed to load movie details");
+    }
+  }
+}
