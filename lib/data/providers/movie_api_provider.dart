e7b8b2dd01ad6a4c0aff0643f131b4a324d46b0a
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-27 19:28:57 +0300

    feat(searchMovie): add search method to fetch movies based on query

--- lib/data/providers/movie_api_provider.dart
+++ lib/data/providers/movie_api_provider.dart
@@ -8,6 +8,7 @@ class MovieApiProvider {
   Future<List<Movie>> fetchTrendingMovies(int page) async {
     final response = await http.get(Uri.parse(
         "https://api.themoviedb.org/3/discover/movie?api_key=${dotenv.env["APIKEY"]}&sort_by=popularity.desc&page=$page"));
+
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       List<Movie> movies = List<Movie>.from(
@@ -22,7 +23,7 @@ class MovieApiProvider {
   }
 
   Future<MovieDetails> fetchMovieDetails(int id) async {
-    await dotenv.load(fileName: ".env");
+    // await dotenv.load(fileName: ".env");
     final response = await http.get(
       Uri.parse(
           "https://api.themoviedb.org/3/movie/$id?api_key=${dotenv.env["APIKEY"]}"),
@@ -33,6 +34,23 @@ class MovieApiProvider {
       return MovieDetails.fromJson(data);
     } else {
       throw Exception("Failed to load movie details");
+    }
+  }
+
+  Future<List<Movie>> searchMovie(String query) async {
+    final response = await http.get(Uri.parse(
+        "https://api.themoviedb.org/3/search/movie?query=$query&api_key=${dotenv.env["APIKEY"]}"));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<Movie> movies = List<Movie>.from(
+        data['results'].map(
+          (movie) => Movie.fromJson(movie),
+        ),
+      );
+      return movies;
+    } else {
+      throw Exception("Failed to fetch movie results");
     }
   }
 }
