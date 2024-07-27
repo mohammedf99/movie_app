ca1c422d5e085c24867aef99dbc1646b8b1e8ec7
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-27 13:47:25 +0300

    fix(MovieListTile): remove unnecessary packages

--- lib/presentation/widgets/movie_list_tile.dart
+++ lib/presentation/widgets/movie_list_tile.dart
@@ -1,8 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/business_logic/blocs/movie_bloc/movie_bloc.dart';
-import 'package:movie_app/presentation/screens/movie_details_screen.dart';
 
 class MovieListTile extends StatelessWidget {
   final int id;
@@ -11,16 +8,20 @@ class MovieListTile extends StatelessWidget {
   final String posterPath;
   final VoidCallback navigationMethod;
 
+  const MovieListTile({
+    super.key,
+    required this.title,
+    required this.releaseDate,
+    required this.posterPath,
+    required this.id,
+    required this.navigationMethod,
+  });
 
-
-  const MovieListTile({super.key, required this.title, required this.releaseDate, required this.posterPath, required this.id, required this.navigationMethod});
-  
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: CachedNetworkImage(
-        imageUrl:
-            'https://image.tmdb.org/t/p/w500$posterPath',
+        imageUrl: 'https://image.tmdb.org/t/p/w500$posterPath',
         placeholder: (context, url) => const CircularProgressIndicator(),
         errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
