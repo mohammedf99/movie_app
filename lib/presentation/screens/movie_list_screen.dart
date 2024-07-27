e7d05f7538b761d381896f9451ccd43d0830746e
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-27 12:02:41 +0300

    feat(movieListScreen): create basic structure of the movie list screen

--- lib/presentation/screens/movie_list_screen.dart
+++ lib/presentation/screens/movie_list_screen.dart
@@ -1,88 +1,15 @@
-// screens/movie_list_screen.dart
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movie_app/business_logic/blocs/movie_bloc/movie_bloc.dart';
-import 'package:movie_app/presentation/screens/movie_details_screen.dart';
 
 class MovieListScreen extends StatefulWidget {
   const MovieListScreen({super.key});
 
   @override
-  _MovieListScreenState createState() => _MovieListScreenState();
+  State<MovieListScreen> createState() => _MovieListScreenState();
 }
 
 class _MovieListScreenState extends State<MovieListScreen> {
-  final ScrollController _scrollController = ScrollController();
-  int _page = 1;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadMovies();
-    _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        _loadMovies();
-      }
-    });
-  }
-
-
-  void _loadMovies() {
-    BlocProvider.of<MovieBloc>(context).add(FetchMoviesEvent(_page));
-    _page++;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Trending Movies'),
-      ),
-      body: BlocBuilder<MovieBloc, MovieState>(
-        builder: (context, state) {
-          if (state is MovieLoading && _page == 1) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is MovieLoaded) {
-            return ListView.builder(
-              controller: _scrollController,
-              itemCount: state.movies.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CachedNetworkImage(
-                    imageUrl:
-                        'https://image.tmdb.org/t/p/w500${state.movies[index].posterPath}',
-                    placeholder: (context, url) =>
-                        const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.error),
-                  ),
-                  title: Text(state.movies[index].title),
-                  subtitle: Text(state.movies[index].releaseDate.split('-')[0]),
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => BlocProvider.value(
-                          value: BlocProvider.of<MovieBloc>(context),
-                          child: MovieDetailsScreen(id: state.movies[index].id),
-                        ),
-                      ),
-                    ).then((_) {
-                      _page--;
-                      _loadMovies();
-                    });
-                  },
-                );
-              },
-            );
-          } else if (state is MovieError) {
-            return Center(child: Text(state.message));
-          } else {
-            return Container();
-          }
-        },
-      ),
-    );
+    return const Placeholder();
   }
 }
