c1a023501f8cdf8b71afc47a21a2ab4f69bca564
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-27 13:44:59 +0300

    feat(MovieListTile): create MovieListTile to reduce code in MovieListScreen

---
+++ lib/presentation/widgets/movie_list_tile.dart
@@ -0,0 +1,32 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/business_logic/blocs/movie_bloc/movie_bloc.dart';
+import 'package:movie_app/presentation/screens/movie_details_screen.dart';
+
+class MovieListTile extends StatelessWidget {
+  final int id;
+  final String title;
+  final String releaseDate;
+  final String posterPath;
+  final VoidCallback navigationMethod;
+
+
+
+  const MovieListTile({super.key, required this.title, required this.releaseDate, required this.posterPath, required this.id, required this.navigationMethod});
+  
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CachedNetworkImage(
+        imageUrl:
+            'https://image.tmdb.org/t/p/w500$posterPath',
+        placeholder: (context, url) => const CircularProgressIndicator(),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+      ),
+      title: Text(title),
+      subtitle: Text(releaseDate.split('-')[0]),
+      onTap: () => navigationMethod(),
+    );
+  }
+}
