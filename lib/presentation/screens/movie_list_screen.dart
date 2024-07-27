3b88d17ea8908a53e97ab645d2d96e9cb8ab9a57
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-27 12:14:41 +0300

    feat(movieListScreen): add different states of movie

--- lib/presentation/screens/movie_list_screen.dart
+++ lib/presentation/screens/movie_list_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_app/business_logic/blocs/movie_bloc/movie_bloc.dart';
 
 class MovieListScreen extends StatefulWidget {
   const MovieListScreen({super.key});
@@ -10,6 +12,28 @@ class MovieListScreen extends StatefulWidget {
 class _MovieListScreenState extends State<MovieListScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trending Movies'),
+      ),
+      body: BlocBuilder<MovieBloc, MovieState>(builder: (context, state) {
+        if (state is MovieLoading) {
+          return const CircularProgressIndicator.adaptive();
+        } else if (state is MovieLoaded) {
+          return Center(
+            child: Text(
+              "Movies loaded",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          );
+        } else if (state is MovieError) {
+          return Center(
+            child: Text(state.message),
+          );
+        } else {
+          return Container();
+        }
+      }),
+    );
   }
 }
