a84dc88a662853ce82ae0d46a9f711365dbd7c2a
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-07-28 14:39:11 +0300

    feat(main): initialize GetStorage for offline times

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:movie_app/business_logic/blocs/genre_bloc/genre_bloc.dart';
 import 'package:movie_app/data/providers/movie_api_provider.dart';
 import 'package:movie_app/data/repositories/movie_repository.dart';
@@ -10,6 +11,7 @@ import 'business_logic/blocs/movie_bloc/movie_bloc.dart';
 import 'presentation/theme/theme.dart';
 
 void main() async {
+  await GetStorage.init();
   await dotenv.load(fileName: ".env");
   final movieApiProvider = MovieApiProvider();
   final movieRepository = MovieRepository(movieApiProvider: movieApiProvider);
